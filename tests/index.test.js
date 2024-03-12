c431ec333adf84a83d2239ec4ed0f77c88dc98f2
ChrisCodesThings <153457755+ChrisCodesThings@users.noreply.github.com>
2024-03-12 21:06:10 +0000

    fix: update test to match input change!

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,6 +3,6 @@ const { default: testFunc } = await import("../");
 
 describe("let's check the value of cornflowerblue", () => {
     test("checking YIQ value", async () => {
-        expect(testFunc(100, 149, 237)).toEqual(144.381);
+        expect(testFunc([100, 149, 237])).toEqual(144.381);
     });
 });
