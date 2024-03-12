999df0e14d2e4ebb3e1671e5d059e15eac39f03f
ChrisCodesThings <153457755+ChrisCodesThings@users.noreply.github.com>
2024-03-12 21:04:52 +0000

    refactor!: change input parameter to rgb in array, add input check

--- src/rgbColorYIQValue.js
+++ src/rgbColorYIQValue.js
@@ -1,4 +1,10 @@
+import looksLikeRGBA from '@chriscodesthings/color-looks-like-rgba';
 
-export default function (r, g, b) {
-    return ((r * 299) + (g * 587) + (b * 114)) / 1000;
+
+export default function (col) {
+    if (!looksLikeRGBA(col)) {
+        return;
+    }
+
+    return ((col[0] * 299) + (col[1] * 587) + (col[2] * 114)) / 1000;
 }
